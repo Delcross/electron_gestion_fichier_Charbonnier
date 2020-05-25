2ad1c53e01da1bb2fb4f1b10c02f4a0ab273b41c
Charbonnier Robin <charbonnierrobin@gmail.com>
2020-05-25 17:50:51 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import FileTree from './Utilities/FileTree';
-var remote = window.require('electron').remote;
-var { dialog } = remote;
 
 class App extends Component { 
   constructor(props){
@@ -15,19 +12,6 @@ class App extends Component {
         <button onClick={this.handleOpenFolder}>Open Folder</button>
       </div>
     );
-  }
-
-  handleOpenFolder = () => {
-    var directory = dialog.showOpenDialog({ properties: ['openDirectory']});
-
-    if (directory && directory[0]){
-      var fileTree = new FileTree(directory[0]);
-
-      fileTree.build();
-
-      console.log(fileTree);
-    }
-
   }
 }
 
